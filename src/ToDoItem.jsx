becdde0424661a2d90ad5b0c36f2e8ca5f62e3eb
chutintorn srisittikum <zzz17244@gmail.com>
2024-03-25 16:55:58 +0700

    homework2

--- src/ToDoItem.jsx
+++ src/ToDoItem.jsx
@@ -4,7 +4,7 @@ import './ToDoItem.css'; // Import CSS file for styling
 export default function ToDoItems(props) {
   return (
     <div className={`todo-item ${props.numbers.priority === 0 ? 'highlightred' : 'todo-itemss'}`}>
-      No#: {props.index + 1} : "{props.numbers.todo}" :  Priority= {props.numbers.priority}
+      Nmuber#: {props.index + 1} : "{props.numbers.todo}" :  Priority= {props.numbers.priority}
     </div>
   );
 }
